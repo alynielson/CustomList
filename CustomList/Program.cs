399e66011f9201db43043824366ff73c09581249
alynielson <nielsonaly@gmail.com>
2018-08-09 14:55:12 -0500

    deleted the testing in the Program file

--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -11,15 +11,7 @@ namespace CustomList
         static void Main(string[] args)
         {
             
-            CustomList<int> ints = new CustomList<int> { 1, 2, 3 };
-            CustomList<int> ints2 = new CustomList<int> { 1, 2, 3 };
-            CustomList<CustomList<int>> firstList = new CustomList<CustomList<int>> {ints,ints2 };
-            firstList.Sort();
-            for (int i = 0; i < firstList.Count; i ++)
-            {
-                Console.WriteLine($"Sorted list at index {i}: {firstList[i]}");
-            }
-            Console.ReadLine();
+            
 
         }
     }
